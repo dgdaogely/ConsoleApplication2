20dff7b90a8ae398f00c1ba7d129890ed9870d00
dgdaogely <56956144+dgdaogely@users.noreply.github.com>
2020-02-20 16:31:07 +0800

    Add files via upload
    My second time to push.

---
+++ ConsoleApplication2.cpp
@@ -0,0 +1,88 @@
+﻿// ConsoleApplication1.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+
+#include <vtkActor.h>
+#include <vtkCamera.h>
+#include <vtkCylinderSource.h>
+#include <vtkNamedColors.h>
+#include <vtkPolyDataMapper.h>
+#include <vtkProperty.h>
+#include <vtkRenderWindow.h>
+#include <vtkRenderWindowInteractor.h>
+#include <vtkRenderer.h>
+#include <vtkSmartPointer.h>
+#include <array>
+#include <vtkAutoInit.h>
+VTK_MODULE_INIT(vtkRenderingOpenGL2);//和官方的例子有些不同，下面解释
+VTK_MODULE_INIT(vtkInteractionStyle);
+int main(int, char* []) {
+    vtkSmartPointer<vtkNamedColors> colors =
+        vtkSmartPointer<vtkNamedColors>::New();
+
+    // Set the background color.
+    std::array<unsigned char, 4> bkg{ {26, 51, 102, 255} };
+    colors->SetColor("BkgColor", bkg.data());
+
+    // This creates a polygonal cylinder model with eight circumferential facets
+    // (i.e, in practice an octagonal prism).
+    vtkSmartPointer<vtkCylinderSource> cylinder =
+        vtkSmartPointer<vtkCylinderSource>::New();
+    cylinder->SetResolution(8);
+
+    // The mapper is responsible for pushing the geometry into the graphics
+    // library. It may also do color mapping, if scalars or other attributes are
+    // defined.
+    vtkSmartPointer<vtkPolyDataMapper> cylinderMapper =
+        vtkSmartPointer<vtkPolyDataMapper>::New();
+    cylinderMapper->SetInputConnection(cylinder->GetOutputPort());
+
+    // The actor is a grouping mechanism: besides the geometry (mapper), it
+    // also has a property, transformation matrix, and/or texture map.
+    // Here we set its color and rotate it around the X and Y axes.
+    vtkSmartPointer<vtkActor> cylinderActor = vtkSmartPointer<vtkActor>::New();
+    cylinderActor->SetMapper(cylinderMapper);
+    cylinderActor->GetProperty()->SetColor(
+        colors->GetColor4d("Tomato").GetData());
+    cylinderActor->RotateX(30.0);
+    cylinderActor->RotateY(-45.0);
+
+    // The renderer generates the image
+    // which is then displayed on the render window.
+    // It can be thought of as a scene to which the actor is added
+    vtkSmartPointer<vtkRenderer> renderer = vtkSmartPointer<vtkRenderer>::New();
+    renderer->AddActor(cylinderActor);
+    renderer->SetBackground(colors->GetColor3d("BkgColor").GetData());
+    // Zoom in a little by accessing the camera and invoking its "Zoom" method.
+    renderer->ResetCamera();
+    renderer->GetActiveCamera()->Zoom(1.5);
+
+    // The render window is the actual GUI window
+    // that appears on the computer screen
+    vtkSmartPointer<vtkRenderWindow> renderWindow =
+        vtkSmartPointer<vtkRenderWindow>::New();
+    renderWindow->SetSize(300, 300);
+    renderWindow->AddRenderer(renderer);
+    renderWindow->SetWindowName("Cylinder");
+
+    // The render window interactor captures mouse events
+    // and will perform appropriate camera or actor manipulation
+    // depending on the nature of the events.
+    vtkSmartPointer<vtkRenderWindowInteractor> renderWindowInteractor =
+        vtkSmartPointer<vtkRenderWindowInteractor>::New();
+    renderWindowInteractor->SetRenderWindow(renderWindow);
+
+    // This starts the event loop and as a side effect causes an initial render.
+    renderWindowInteractor->Start();
+
+    return EXIT_SUCCESS;
+}
+// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
+// 调试程序: F5 或调试 >“开始调试”菜单
+
+// 入门使用技巧: 
+//   1. 使用解决方案资源管理器窗口添加/管理文件
+//   2. 使用团队资源管理器窗口连接到源代码管理
+//   3. 使用输出窗口查看生成输出和其他消息
+//   4. 使用错误列表窗口查看错误
+//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
+//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
